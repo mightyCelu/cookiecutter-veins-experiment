150b182e66ee1a51da22e5dd2d1c4223434bc4ef
Max Schettler <schettler@ccs-labs.org>
2019-09-30 15:00:52 +0200

    Add Plexe dependency

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,3 +7,10 @@ subprocess.call(['git', 'init'], cwd='{{ cookiecutter.veins.path }}')
 subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.veins.repository }}'], cwd='{{ cookiecutter.veins.path }}')
 subprocess.call(['git', 'fetch'], cwd='{{ cookiecutter.veins.path }}')
 subprocess.call(['git', 'checkout', '{{ cookiecutter.veins.version }}'], cwd='{{ cookiecutter.veins.path }}')
+
+# Plexe
+pathlib.Path('{{ cookiecutter.plexe.path }}').mkdir(parents=True)
+subprocess.call(['git', 'init'], cwd='{{ cookiecutter.plexe.path }}')
+subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.plexe.repository }}'], cwd='{{ cookiecutter.plexe.path }}')
+subprocess.call(['git', 'fetch'], cwd='{{ cookiecutter.plexe.path }}')
+subprocess.call(['git', 'checkout', '{{ cookiecutter.plexe.version }}'], cwd='{{ cookiecutter.plexe.path }}')
