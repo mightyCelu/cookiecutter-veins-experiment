a364f485d19dde0437670b8caa53534470d16c50
Dominik S. Buse <buse@ccs-labs.org>
2020-07-09 14:10:03 +0200

    Add config for veins-vlc.

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Creates a [Veins](https://veins.car2x.org) experiment. Veins is cloned into a su
 
     cookiecutter cookiecutter-veins-experiment
 
-This repository supports different experiment types, at the moment 'Veins' (default) and 'Plexe'.
+This repository supports different experiment types, at the moment 'Veins' (default) and 'Veins-VLC'.
 These can be used by specifying the corresponding branches, e.g.:
 
-    cookiecutter cookiecutter-veins-experiment -c plexe
+    cookiecutter cookiecutter-veins-experiment -c vlc
 
 After this, the directory is set up, you only need to add experiment configuration to `scenario`. Then, run:
 
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,3 +7,10 @@ subprocess.call(['git', 'init'], cwd='{{ cookiecutter.veins.path }}')
 subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.veins.repository }}'], cwd='{{ cookiecutter.veins.path }}')
 subprocess.call(['git', 'fetch'], cwd='{{ cookiecutter.veins.path }}')
 subprocess.call(['git', 'checkout', '{{ cookiecutter.veins.version }}'], cwd='{{ cookiecutter.veins.path }}')
+
+# Veins-VLC
+pathlib.Path('{{ cookiecutter.vlc.path }}').mkdir(parents=True)
+subprocess.call(['git', 'init'], cwd='{{ cookiecutter.vlc.path }}')
+subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.vlc.repository }}'], cwd='{{ cookiecutter.vlc.path }}')
+subprocess.call(['git', 'fetch'], cwd='{{ cookiecutter.vlc.path }}')
+subprocess.call(['git', 'checkout', '{{ cookiecutter.vlc.version }}'], cwd='{{ cookiecutter.vlc.path }}')
